a1f3b446add47520dc010a228efeda839d17442e
dinuvinethumila <153153798+dinuvinethumila@users.noreply.github.com>
2025-02-02 18:31:01 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = "shopcart-g6b6exbrddb2h4hw.canadacentral-01.azurewebsites.net";
+export const API_URL = "https://shopcart-g6b6exbrddb2h4hw.canadacentral-01.azurewebsites.net/";

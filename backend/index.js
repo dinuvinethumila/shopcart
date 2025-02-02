cf49dbcb341af98c0d26b10be7956b6d5946da27
dinuvinethumila <153153798+dinuvinethumila@users.noreply.github.com>
2025-02-02 17:29:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const Routes = require("./routes/route.js");
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGO_URL; // Changed to MONGO_URl
-const FRONTEND_URL = process.env.FRONTEND_URL || "*"; // Allow frontend access
+const FRONTEND_URL = process.env.FRONTEND_URL || "https://lively-tree-01f0fd50f.4.azurestaticapps.net"; // Allow frontend access
 
 // Middleware
 app.use(express.json({ limit: '10mb' }));
@@ -18,6 +18,7 @@ app.use(express.json({ limit: '10mb' }));
 // CORS Configuration: Allows only your frontend app
 app.use(cors({
     origin: FRONTEND_URL,
+     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true // Enable if using authentication (JWT, cookies, etc.)
 }));
 

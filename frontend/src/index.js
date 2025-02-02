31edbef0de117cf3e1b1ee4f8e7be5f615859ca2
dinuvinethumila <153153798+dinuvinethumila@users.noreply.github.com>
2025-02-02 16:01:49 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,11 @@ import './index.css'
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
 
+fetch('shopcart-g6b6exbrddb2h4hw.canadacentral-01.azurewebsites.net')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error("Error:", error));
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
